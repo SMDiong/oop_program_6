4fce8fe7d55c60bc5da8a955f6fcfc039ffd0481
Shan Marc Diong <163641792+Zullivann@users.noreply.github.com>
2024-05-24 01:58:52 +0800

    Created the first TV 1 object then added the required conditions

--- TestTV.py
+++ TestTV.py
@@ -2,12 +2,23 @@
 # BSCPE 1-2
 
 # Import TV
+from TV import TV
+
 
 # Create the test driver program that will create two objects from Class TV
+def test_tv():
+    # Creating the first TV object
+    tv_1 = TV()
+    # Turn on TV 1
+    tv_1.turn_on()
+    # Set volume of TV 1 to 3
+    tv_1.set_volume(3)
+    # Set channel of TV 1 to 30
+    tv_1.set_channel(30)
 
-# Creating the first TV object
+    # Creating the second TV object
 
-# Creating the second TV object
+    # Output results of both TV objects
 
-# Output results of both TV objects
 
+test_tv()
