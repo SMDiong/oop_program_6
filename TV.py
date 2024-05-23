9a9d8398bad15f2192f32c76cbb7ba4502507da6
Shan Marc Diong <163641792+Zullivann@users.noreply.github.com>
2024-05-24 01:08:10 +0800

    Added pseudocodes on both TV and TestTV programs

---
+++ TV.py
@@ -0,0 +1,24 @@
+# Diong, Shan Marc C.
+# BSCPE 1-2
+
+# Create the class named TV
+
+# Constructor for channel, volume level, and on
+
+# Creating method for turn on and off
+
+# Creating method for getting the channel
+
+# Creating method for setting the channel ranging from 1 to 120
+
+# Creating method for getting the volume
+
+# Creating method for setting the volume ranging from 1 to 7
+
+# Creating method for switching up the channel
+
+# Creating method for switching down the channel
+
+# Creating method for increasing volume
+
+# Creating method for decreasing volume
---
+++ TestTV.py
@@ -0,0 +1,13 @@
+# Diong, Shan Marc C.
+# BSCPE 1-2
+
+# Import TV
+
+# Create the test driver program that will create two objects from Class TV
+
+# Creating the first TV object
+
+# Creating the second TV object
+
+# Output results of both TV objects
+
