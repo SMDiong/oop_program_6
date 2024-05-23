5008cd67827206ecb4fc1345291b1d6d6c2f19dd
Shan Marc Diong <163641792+Zullivann@users.noreply.github.com>
2024-05-24 01:14:48 +0800

    Created the constructor for channel, volume level, and on

--- TV.py
+++ TV.py
@@ -2,23 +2,27 @@
 # BSCPE 1-2
 
 # Create the class named TV
+class TV:
+    # Constructor for channel, volume level, and on
+    def __init__(self):
+        self.channel = int
+        self.volume_level = int
+        self.on = False
 
-# Constructor for channel, volume level, and on
+    # Creating method for turn on and off
 
-# Creating method for turn on and off
+    # Creating method for getting the channel
 
-# Creating method for getting the channel
+    # Creating method for setting the channel ranging from 1 to 120
 
-# Creating method for setting the channel ranging from 1 to 120
+    # Creating method for getting the volume
 
-# Creating method for getting the volume
+    # Creating method for setting the volume ranging from 1 to 7
 
-# Creating method for setting the volume ranging from 1 to 7
+    # Creating method for switching up the channel
 
-# Creating method for switching up the channel
+    # Creating method for switching down the channel
 
-# Creating method for switching down the channel
+    # Creating method for increasing volume
 
-# Creating method for increasing volume
-
-# Creating method for decreasing volume
+    # Creating method for decreasing volume
